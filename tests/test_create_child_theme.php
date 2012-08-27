dbb1f99b5915804378c8397aba29e540df8bacd6
John P. Bloch <john@avendimedia.com>
2012-08-27 17:37:28 -0400

    Tweak the test case for creating a child theme

--- tests/test_create_child_theme.php
+++ tests/test_create_child_theme.php
@@ -1,25 +1,33 @@
 <?php
-/**
- * Tests to test that that testing framework is testing tests. Meta, huh?
- * @package wordpress-plugins-tests
- */
+
 class WP_Test_Create_Child_Theme extends WP_UnitTestCase {
 
-	public function setUp(){
+	/**
+	 * @var WP_Theme Theme object
+	 */
+	protected $theme;
+
+	public function setUp() {
 		parent::setUp();
 	}
 
-	public function test_create_child_theme(){
+	public function test_create_child_theme() {
 		$current_theme = wp_get_theme();
 		$random = wp_generate_password( 6, false );
-		$new_theme_name = basename( $current_theme->stylesheet_dir );
+		$new_theme_name = basename( $current_theme->stylesheet_dir ) . '_' . $random;
 		$this->assertFalse( $current_theme->parent() );
 		CTF_BabyMaker::procreate( $new_theme_name, $current_theme );
-		$new_theme = wp_get_theme( $new_theme_name );
+		$this->theme = $new_theme = wp_get_theme( $new_theme_name );
 		$this->assertTrue( $new_theme->exists() );
 		$this->assertEquals( $new_theme->parent(), $current_theme );
 		$files = $new_theme->get_files();
 		$this->assertEquals( count( $files ), 1 );
+	}
+
+	public function tearDown() {
+		$themeDir = $this->theme->get_stylesheet_directory();
+		`rm -r $themeDir`;
+		parent::tearDown();
 	}
 
 }
