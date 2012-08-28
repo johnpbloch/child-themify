5f3d824011ac4baad1a3f3e366cc683c7f2e5fd2
John P. Bloch <john@avendimedia.com>
2012-08-28 16:21:08 -0400

    Add the class and methods from the tests.

--- child-themify.php
+++ child-themify.php
@@ -1,6 +1,31 @@
 <?php
+
 /*
  * Plugin Name: Child Themify
  * Description: Enables the quick creation of child themes from any non-child theme you have installed.
  * Version: 0.1-alpha
  */
+
+class CTF_Babymaker {
+
+	public static function getTested() {
+
+	}
+
+	public static function showInterface() {
+
+	}
+
+	public static function getLink() {
+
+	}
+
+	public static function moodLighting() {
+
+	}
+
+	public static function procreate() {
+		
+	}
+
+}
