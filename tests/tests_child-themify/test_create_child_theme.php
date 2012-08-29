dae67faa74ee4584715d959d30d5ecd62f20494c
John P. Bloch <johnpbloch@gmail.com>
2012-08-29 12:52:53 -0400

    Update the unit test to run in its own process. This is so we can define a constant without it showing up in our other tests.

--- tests/tests_child-themify/test_create_child_theme.php
+++ tests/tests_child-themify/test_create_child_theme.php
@@ -7,27 +7,24 @@ class WP_Test_Create_Child_Theme extends WP_UnitTestCase {
 	 */
 	protected $theme;
 
-	public function setUp() {
-		parent::setUp();
-	}
-
+	/**
+	 * @runInSeparateProcess
+	 */
 	public function test_create_child_theme() {
+		define('FS_METHOD', 'direct');
 		$current_theme = wp_get_theme();
 		$random = wp_generate_password( 6, false );
 		$new_theme_name = basename( $current_theme->stylesheet_dir ) . '_' . $random;
+		$new_theme_name = sanitize_file_name( $new_theme_name );
 		$this->assertFalse( $current_theme->parent() );
 		CTF_BabyMaker::procreate( $new_theme_name, $current_theme );
-		$this->theme = $new_theme = wp_get_theme( $new_theme_name );
-		$this->assertTrue( $new_theme->exists() );
-		$this->assertEquals( $new_theme->parent(), $current_theme );
-		$files = $new_theme->get_files();
+		$this->theme = wp_get_theme( $new_theme_name );
+		$this->assertTrue( $this->theme->exists() );
+		$this->assertEquals( $this->theme->parent()->get_stylesheet(), $current_theme->get_stylesheet() );
+		$files = $this->theme->get_files();
 		$this->assertEquals( count( $files ), 1 );
-	}
-
-	public function tearDown() {
 		$themeDir = $this->theme->get_stylesheet_directory();
 		_rmdir( $themeDir );
-		parent::tearDown();
 	}
 
 }
