3eaca1aa0beea7fd1b128da199ccbecad044bf09
John P. Bloch <johnpbloch@gmail.com>
2012-12-31 12:12:19 -0500

    Add more plugin headers
    [ci skip]

--- child-themify.php
+++ child-themify.php
@@ -3,6 +3,9 @@
  * Plugin Name: Child Themify
  * Description: Enables the quick creation of child themes from any non-child theme you have installed.
  * Version: 0.1-alpha
+ * Plugin URI: https://github.com/johnpbloch/child-themify
+ * Author: John P. Bloch
+ * License: GPLv2 or later
  */
 
 class CTF_Babymaker {
