27a7f108c6477c78598476ae4c87c4142a10433b
John P. Bloch <john.bloch@10up.com>
2014-01-11 02:22:26 -0500

    Rewrite most of main.js

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,32 +1,51 @@
-(function (window, l10n, undefined) {
-	if (typeof l10n.link !== 'string' || l10n.link.length < 1) {
-		return;
+(function (window, l10n) {
+	var $ = window.jQuery,
+			themes = window.wp.themes,
+			themesView,
+			themeViews = {};
+
+	function injectLinks(into, model) {
+		var className = '.theme-actions .' + (model.get('active') ? '' : 'in') + 'active-theme',
+				links = into.find(className);
+		if (links.length) {
+			links.first().append('<a href="#" class="button button-secondary">' +
+					l10n.createAChildTheme +
+					'</a>');
+		}
 	}
-	var themeOptions = document.getElementById('customize-current-theme-link'),
-			x,
-			optionsLinks,
-			newListItem,
-			newLink;
-	if (themeOptions) {
-		themeOptions = themeOptions.parentNode;
-	} else {
-		return;
+
+	function onExpand() {
+		window.setTimeout(function () {
+			injectLinks(themesView.overlay.$el, themesView.model);
+		}, 30);
 	}
-	for (x in themeOptions.childNodes) {
-		if (themeOptions.childNodes[x].nodeName !== undefined) {
-			if (themeOptions.childNodes[x].nodeName.toUpperCase() === 'UL') {
-				optionsLinks = themeOptions.childNodes[x];
-				break;
+
+	function onLoad() {
+		themesView = themes.Run.view.view;
+		var index,
+				obj,
+				listeners = themesView._listeners,
+				count = 0;
+		for (index in listeners) {
+			if (listeners.hasOwnProperty(index)) {
+				obj = listeners[index];
+				if (obj instanceof themes.view.Theme) {
+					themeViews[obj.model.id] = obj;
+					count += 1;
+				}
+			}
+		}
+
+		if (count === 1) {
+			injectLinks(themesView.singleTheme.$el, themesView.model);
+		} else if (count > 0) {
+			for (index in themeViews) {
+				if (themeViews.hasOwnProperty(index)) {
+					themesView.listenTo(themeViews[index], 'theme:expand', onExpand);
+				}
 			}
 		}
 	}
-	if (!optionsLinks) {
-		return;
-	}
-	newLink = document.createElement('a');
-	newLink.appendChild(document.createTextNode(l10n.createAChildTheme));
-	newLink.href = l10n.link;
-	newListItem = document.createElement('li');
-	newListItem.appendChild(newLink);
-	optionsLinks.appendChild(newListItem);
+
+	$(onLoad);
 }(this, this.childThemify));
