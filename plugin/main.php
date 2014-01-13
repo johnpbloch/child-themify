edb43f7f98e5c9d4a35af70fb5e413dcfb3427c9
John P. Bloch <john.bloch@10up.com>
2014-01-13 10:49:32 -0500

    Clip the filename off CTF_PATH before using it

--- plugin/main.php
+++ plugin/main.php
@@ -3,11 +3,11 @@
 function ctf_plugins_loaded() {
 	if ( version_compare( $GLOBALS['wp_version'], '3.8', '>=' ) ) {
 		global $child_themify;
-		require_once CTF_PATH . '/includes/plugin.php';
+		require_once dirname( CTF_PATH ) . '/includes/plugin.php';
 		$child_themify = new Child_Themify();
 		add_action( 'init', array( $child_themify, 'init' ) );
 	} else {
-		require_once CTF_PATH . '/includes/legacy.php';
+		require_once dirname( CTF_PATH ) . '/includes/legacy.php';
 		add_action( 'init', array( 'CTF_Babymaker', 'init' ) );
 	}
 }
