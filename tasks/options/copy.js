040cb1dd680464dca8986f04ec088575d02c42c9
John P. Bloch <john.bloch@10up.com>
2014-01-14 09:49:07 -0500

    Add a copy:tmp task to copy the plugin to a temporary directory

--- tasks/options/copy.js
+++ tasks/options/copy.js
@@ -1,39 +1,58 @@
 /* jshint node:true */
-module.exports = {
-	files: {
-		files: [
-			{
-				dot   : false,
-				expand: true,
-				cwd   : '.',
-				src   : [
-					'**',
-					// No source control should be copied
-					'!**/.git**/**',
-					// No Github files should be copied
-					'!**/README.md',
-					// No Grunt stuff should get copied
-					'!**/node_modules/**',
-					'!**/package.json',
-					'!**/Gruntfile.js',
-					// No composer stuff should get copied
-					'!**/vendor/**',
-					'!**/composer.json',
-					'!**/composer.lock',
-					'!**/composer.phar',
-					// No PHPUnit stuff should get copied
-					'!phpunit.xml**',
-					'!bootstrap.php',
-					'!tests/**',
-					// Only the compiled js
-					'!assets/js/{src,vendor}/**',
-					// Don't copy the build folder into itself
-					'!build/**',
-					// Don't copy the main plugin file's parts
-					'!plugin/**'
-				],
-				dest  : 'build/trunk/'
-			}
-		]
-	}
-};
+module.exports = (function () {
+	var SRC = [
+				'**',
+				// No source control should be copied
+				'!**/.git**/**',
+				// No Github files should be copied
+				'!**/README.md',
+				// No Grunt stuff should get copied
+				'!**/node_modules/**',
+				'!/tasks/**',
+				'!**/package.json',
+				'!**/Gruntfile.js',
+				// No composer stuff should get copied
+				'!**/vendor/**',
+				'!**/composer.json',
+				'!**/composer.lock',
+				'!**/composer.phar',
+				// No PHPUnit stuff should get copied
+				'!phpunit.xml**',
+				'!bootstrap.php',
+				'!tests/**',
+				// Only the compiled js
+				'!assets/js/{src,vendor}/**',
+				// Don't copy the build folder into itself
+				'!build/**',
+				// Don't copy the main plugin file's parts
+				'!plugin/**'
+			],
+			temp = require('temp');
+
+	temp.track();
+
+	return {
+		files: {
+			files: [
+				{
+					dot   : false,
+					expand: true,
+					cwd   : '.',
+					src   : SRC,
+					dest  : 'build/trunk/'
+				}
+			]
+		},
+		tmp  : {
+			files: [
+				{
+					dot   : false,
+					expand: true,
+					cwd   : '.',
+					src   : SRC,
+					dest  : temp.mkdirSync()
+				}
+			]
+		}
+	};
+}());
