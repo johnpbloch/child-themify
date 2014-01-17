a103f7963dbd060f2a4aca6138edc084b25ac768
John P. Bloch <john.bloch@10up.com>
2014-01-17 02:39:28 -0500

    Add a watcher for php files

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -1,10 +1,14 @@
 /* jshint node:true */
 module.exports = {
-	grunt: {
+	grunt : {
 		files: ['Gruntfile.js', 'tasks/options/**/*.js'],
 		tasks: ['jshint:grunt']
 	},
-	src  : {
+	plugin: {
+		files: ['<%= phplint.plugin %>'],
+		tasks: ['phplint:plugin', 'concat:plugin']
+	},
+	src   : {
 		files: ['assets/js/src/**/*.js'],
 		tasks: ['jshint:ctf', 'concat:childthemify', 'uglify:childthemify']
 	}
