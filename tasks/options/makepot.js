8d6067848695dc377f3ede5896e3bea73165b1f7
John P Bloch <me@johnpbloch.com>
2014-09-20 21:45:29 -0600

    Fix the l18n task to be cross-platform

--- tasks/options/makepot.js
+++ tasks/options/makepot.js
@@ -1,10 +1,15 @@
 /* jshint node:true */
+var os = require('os'),
+		fs = require('fs');
 module.exports = (function () {
+	var file = fs.realpathSync('vendor/bin/makepot.php');
+	if (/^win/.test(os.platform())) {
+		file += '.bat';
+	}
 	return {
 		'default': {
-			cmd : 'php',
+			cmd : file,
 			args: [
-				'vendor/bin/makepot.php',
 				'wp-plugin',
 				'<%= copy.tmpDir %>',
 				'languages/child-themify.pot'
