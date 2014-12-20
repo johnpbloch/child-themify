a213ebcd390bf45b3992f407c6b0606f5529251a
John P Bloch <me@johnpbloch.com>
2014-12-20 10:11:02 -0600

    Remove analytics code

--- plugin/analytics.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function ctf_do_analytics() {
-	require dirname( CTF_PATH ) . '/includes/analytics.php';
-	$analytics = new CTF_Analytics();
-	$analytics->run();
-}
-
-add_action( 'plugins_loaded', 'ctf_do_analytics', 9 );
