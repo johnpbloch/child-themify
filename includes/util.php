4ee518cca01ac4da4f8310e1067d2dd8cea22ff8
John P Bloch <me@johnpbloch.com>
2017-07-19 09:05:22 -0500

    Add some utility functions

--- includes/util.php
+++ includes/util.php
@@ -1,1 +1,22 @@
 <?php
+
+function child_themify_asset( $type, $file ) {
+	return esc_url_raw( sprintf(
+		'%sassets/%s/%s',
+		CTF_URL,
+		( 'js' === $type ) ? 'js' : 'css',
+		$file
+	) );
+}
+
+function child_themify_js( $file = 'child-themify.js' ) {
+	return child_themify_asset( 'js', $file );
+}
+
+function child_themify_css( $file = 'child-themify.css' ) {
+	return child_themify_asset( 'css', $file );
+}
+
+function child_themify_asset_version() {
+	return '{{VERSION}}' === CTF_VERSION ? time() : CTF_VERSION;
+}
