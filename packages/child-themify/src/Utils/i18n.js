34e0fd83bbd9d90ac363ba3ba64d458b90d3151f
John P Bloch <me@johnpbloch.com>
2017-08-30 12:58:50 -0500

    Get coverage to stop complaning about the default values

--- packages/child-themify/src/Utils/i18n.js
+++ packages/child-themify/src/Utils/i18n.js
@@ -1,3 +1,3 @@
-const i18n = window.ChildThemify.i18n || {};
+const i18n = window.ChildThemify.i18n || /* istanbul ignore next */ {};
 
 export {i18n};
--- packages/child-themify/src/Utils/settings.js
+++ packages/child-themify/src/Utils/settings.js
@@ -1,9 +1,9 @@
-window.ChildThemify = window.ChildThemify || {};
+window.ChildThemify = window.ChildThemify || /* istanbul ignore next */ {};
 
-const rest_url = window.ChildThemify.rest || '';
-const rest_nonce = window.ChildThemify.nonce || '';
-const theme_list = window.ChildThemify.themes || [];
-const current_user = window.ChildThemify.current_user || '';
+const rest_url = window.ChildThemify.rest || /* istanbul ignore next */ '';
+const rest_nonce = window.ChildThemify.nonce || /* istanbul ignore next */ '';
+const theme_list = window.ChildThemify.themes || /* istanbul ignore next */ [];
+const current_user = window.ChildThemify.current_user || /* istanbul ignore next */ '';
 
 const settings = {current_user, rest_url, rest_nonce, theme_list};
 
