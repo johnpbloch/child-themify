d9e445651a20a2d2886b4a684f78e6cb421f1164
John P Bloch <me@johnpbloch.com>
2017-08-31 08:05:56 -0500

    Wrap app tests in a suite

--- packages/child-themify/src/__tests__/App.test.js
+++ packages/child-themify/src/__tests__/App.test.js
@@ -5,69 +5,71 @@ import {shallow, mount} from 'enzyme';
 import state from '../../__mocks__/state.json';
 import App from '../App';
 
-const axiosMock = new AxiosMockAdapter(axios);
+describe('<App/> component tests', () => {
+    const axiosMock = new AxiosMockAdapter(axios);
 
-test('Name field appears when theme selected', () => {
-    const component = shallow(<App themes={state.themes}/>);
+    test('Name field appears when theme selected', () => {
+        const component = shallow(<App themes={state.themes}/>);
 
-    expect(component.find('.name-field-wrapper').exists()).toBe(false);
+        expect(component.find('.name-field-wrapper').exists()).toBe(false);
 
-    axiosMock.reset();
-    axiosMock
-        .onGet('http://ctf.dev/wp-json/child-themify/v1/theme-data/twentyseventeen')
-        .reply(200, import('../../__mocks__/api/theme-data/twentyseventeen.json'));
+        axiosMock.reset();
+        axiosMock
+            .onGet('http://ctf.dev/wp-json/child-themify/v1/theme-data/twentyseventeen')
+            .reply(200, import('../../__mocks__/api/theme-data/twentyseventeen.json'));
 
-    component.instance().selectTheme({value: 'twentyseventeen'});
+        component.instance().selectTheme({value: 'twentyseventeen'});
 
-    expect(component.find('.name-field-wrapper').exists()).toBe(true);
-});
+        expect(component.find('.name-field-wrapper').exists()).toBe(true);
+    });
 
-test('Name converts to predictable slug', () => {
-    const component = shallow(<App themes={state.themes}/>);
-    const instance = component.instance();
-    instance.checkChildSlug = instance.realCheckChildSlug;
-    axiosMock.reset();
-    axiosMock
-        .onGet('http://ctf.dev/wp-json/child-themify/v1/theme-data/twentyseventeen')
-        .reply(200, import('../../__mocks__/api/theme-data/twentyseventeen.json'))
-        .onGet()
-        .reply(404, {});
-    instance.selectTheme({value: 'twentyseventeen'});
+    test('Name converts to predictable slug', () => {
+        const component = shallow(<App themes={state.themes}/>);
+        const instance = component.instance();
+        instance.checkChildSlug = instance.realCheckChildSlug;
+        axiosMock.reset();
+        axiosMock
+            .onGet('http://ctf.dev/wp-json/child-themify/v1/theme-data/twentyseventeen')
+            .reply(200, import('../../__mocks__/api/theme-data/twentyseventeen.json'))
+            .onGet()
+            .reply(404, {});
+        instance.selectTheme({value: 'twentyseventeen'});
 
-    instance.updateThemeName('Basic Name');
-    expect(component.state('childSlug')).toBe('basic-name');
+        instance.updateThemeName('Basic Name');
+        expect(component.state('childSlug')).toBe('basic-name');
 
-    instance.updateThemeName('Name&with!symbols*#($*%');
-    expect(component.state('childSlug')).toBe('namewithsymbols');
+        instance.updateThemeName('Name&with!symbols*#($*%');
+        expect(component.state('childSlug')).toBe('namewithsymbols');
 
-    instance.updateThemeName('Preserves-hyphens_underscores, not~other/punctuation.');
-    expect(component.state('childSlug')).toBe('preserves-hyphens_underscores-nototherpunctuation');
+        instance.updateThemeName('Preserves-hyphens_underscores, not~other/punctuation.');
+        expect(component.state('childSlug')).toBe('preserves-hyphens_underscores-nototherpunctuation');
 
-    instance.updateThemeName(`handles\twhite\nspace well`);
-    expect(component.state('childSlug')).toBe('handles-white-space-well');
+        instance.updateThemeName(`handles\twhite\nspace well`);
+        expect(component.state('childSlug')).toBe('handles-white-space-well');
 
-    instance.updateThemeName(`compresses
+        instance.updateThemeName(`compresses
     redundant               whitespace
     \t of all kinds`);
-    expect(component.state('childSlug')).toBe('compresses-redundant-whitespace-of-all-kinds');
+        expect(component.state('childSlug')).toBe('compresses-redundant-whitespace-of-all-kinds');
 
-    instance.updateThemeName('compresses----hyphens_______underscores___---___separately');
-    expect(component.state('childSlug')).toBe('compresses-hyphens_underscores_-_separately');
-});
+        instance.updateThemeName('compresses----hyphens_______underscores___---___separately');
+        expect(component.state('childSlug')).toBe('compresses-hyphens_underscores_-_separately');
+    });
 
-test('Advanced field toggle works', () => {
-    const component = mount(<App themes={state.themes}/>);
+    test('Advanced field toggle works', () => {
+        const component = mount(<App themes={state.themes}/>);
 
-    axiosMock.reset();
-    axiosMock
-        .onGet('http://ctf.dev/wp-json/child-themify/v1/theme-data/twentyseventeen')
-        .reply(200, import('../../__mocks__/api/theme-data/twentyseventeen.json'));
+        axiosMock.reset();
+        axiosMock
+            .onGet('http://ctf.dev/wp-json/child-themify/v1/theme-data/twentyseventeen')
+            .reply(200, import('../../__mocks__/api/theme-data/twentyseventeen.json'));
 
-    component.instance().selectTheme({value: 'twentyseventeen'});
+        component.instance().selectTheme({value: 'twentyseventeen'});
 
-    expect(component.find('ExtraFiles').exists()).toBe(false);
+        expect(component.find('ExtraFiles').exists()).toBe(false);
 
-    component.find('.advancedToggle').simulate('click');
+        component.find('.advancedToggle').simulate('click');
 
-    expect(component.find('ExtraFiles').exists()).toBe(true);
+        expect(component.find('ExtraFiles').exists()).toBe(true);
+    });
 });
