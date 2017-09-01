52748358e0ce3b6647d133259502a283510fed52
John P Bloch <me@johnpbloch.com>
2017-09-01 17:24:42 -0500

    Add credentials to settings

--- packages/child-themify/src/Utils/settings.js
+++ packages/child-themify/src/Utils/settings.js
@@ -4,7 +4,8 @@ const rest_url = window.ChildThemify.rest || /* istanbul ignore next */ '';
 const rest_nonce = window.ChildThemify.nonce || /* istanbul ignore next */ '';
 const theme_list = window.ChildThemify.themes || /* istanbul ignore next */ [];
 const current_user = window.ChildThemify.current_user || /* istanbul ignore next */ '';
+const credentials = window.ChildThemify.creds || /* istanbul ignore next */ {};
 
-const settings = {current_user, rest_url, rest_nonce, theme_list};
+const settings = {current_user, rest_url, rest_nonce, theme_list, credentials};
 
 export {settings};
