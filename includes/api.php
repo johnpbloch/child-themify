0db43e0909f9ca53811eab8d2395e9f213331fa6
John P Bloch <me@johnpbloch.com>
2017-09-01 16:51:10 -0500

    Add API endpoint to create theme

--- includes/api.php
+++ includes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+/**
+ * API setup
+ */
 function child_themify_api_init() {
 	add_action( 'rest_api_init', 'child_themify_api_endpoints' );
 }
 
 /**
+ * Register custom endpoints
+ *
  * @param WP_REST_Server $server
  */
 function child_themify_api_endpoints( $server ) {
@@ -18,10 +23,44 @@ function child_themify_api_endpoints( $server ) {
 		),
 		'permission_callback' => 'child_themify_api_permissions',
 	) );
+	register_rest_route( 'child-themify/v1', 'create-theme', array(
+		'methods'             => WP_REST_Server::CREATABLE,
+		'callback'            => 'child_themify_api_create_theme',
+		'args'                => array(
+			'slug'        => array(
+				'sanitize_callback' => 'sanitize_file_name',
+				'validate_callback' => 'child_themify_validate_slug',
+				'required'          => true,
+			),
+			'parent'      => array(
+				'sanitize_callback' => 'child_themify_api_sanitize_theme',
+				'validate_callback' => 'child_themify_api_validate_parent',
+				'required'          => true,
+			),
+			'name'        => array(
+				'sanitize_callback' => 'sanitize_text_field',
+			),
+			'author'      => array(
+				'sanitize_callback' => 'sanitize_text_field',
+			),
+			'extra_files' => array(
+				'sanitize_callback' => 'child_themify_sanitize_extra_files',
+				'default'           => array(),
+			),
+			'creds'       => array(
+				'default' => array(),
+			)
+		),
+		'permission_callback' => 'child_themify_api_permissions',
+	) );
 }
 
 /**
+ * Get a list of theme files for the React app
+ *
  * @param WP_REST_Request $request
+ *
+ * @return WP_Error|WP_REST_Response
  */
 function child_themify_api_get_theme_files( $request ) {
 	if ( ! $request['theme'] || ! $request['theme'] instanceof WP_Theme ) {
@@ -45,12 +84,102 @@ function child_themify_api_get_theme_files( $request ) {
 	return rest_ensure_response( array( 'files' => $files ) );
 }
 
+/**
+ * Turn a slug into a theme object if the theme exists
+ *
+ * @param $maybe_theme
+ *
+ * @return null|WP_Theme
+ */
 function child_themify_api_sanitize_theme( $maybe_theme ) {
 	$theme = wp_get_theme( $maybe_theme );
 
 	return $theme->exists() ? $theme : null;
 }
 
+/**
+ * Create a child theme
+ *
+ * @param WP_REST_Request $request
+ *
+ * @return WP_Error|WP_REST_Response
+ */
+function child_themify_api_create_theme( $request ) {
+	$parent = $request['parent'];
+	$parent = $parent instanceof WP_Theme ? $parent : wp_get_theme( $parent );
+
+	try {
+		child_themify_create_theme(
+			$request['theme'],
+			$parent,
+			$request['name'] ? $request['name'] : $request['slug'],
+			$request['author'],
+			$request['extra_files'],
+			$request['creds']
+		);
+	} catch ( Exception $e ) {
+		$status = $e->getCode() === 1 ? 400 : 500;
+		$codes  = array(
+			1 => 'ctf_no_fs',
+			2 => 'ctf-no-create-dir',
+			3 => 'ctf-no-create-stylesheet',
+			4 => 'ctf-no-write-stylesheet',
+			5 => 'ctf-no-create-functions',
+		);
+		$code   = empty( $codes[ $e->getCode() ] ) ? 'ctf-error' : $codes[ $e->getCode() ];
+
+		return new WP_Error( $code, $e->getMessage(), compact( 'status' ) );
+	}
+
+	return rest_ensure_response( array( 'success' => true ) );
+}
+
+/**
+ * Validate the child slug by making sure it doesn't already exist
+ *
+ * @param $slug
+ *
+ * @return bool
+ */
+function child_themify_validate_slug( $slug ) {
+	$theme = wp_get_theme( $slug );
+
+	return ! $theme->exists();
+}
+
+/**
+ * Validate a parent by making sure it exists and isn't a child theme
+ *
+ * @param $parent
+ *
+ * @return bool
+ */
+function child_themify_api_validate_parent( $parent ) {
+	if ( ! ( $parent instanceof WP_Theme ) ) {
+		$parent = wp_get_theme( $parent );
+	}
+
+	return $parent->exists() && ! $parent->parent();
+}
+
+/**
+ * Run the array of extra files through sanitize_text_field
+ *
+ * @param string[] $files
+ *
+ * @return array
+ */
+function child_themify_sanitize_extra_files( $files ) {
+	$files = (array) $files;
+
+	return array_map( 'sanitize_text_field', $files );
+}
+
+/**
+ * Check whether the current user can use the plugin's api endpoints
+ *
+ * @return bool
+ */
 function child_themify_api_permissions() {
 	return current_user_can( 'install_themes' );
 }
--- includes/util.php
+++ includes/util.php
@@ -101,3 +101,172 @@ function child_themify_get_fs_object( $creds ) {
 
 	return false;
 }
+
+/**
+ * @param string $slug
+ * @param WP_Theme $parent
+ * @param string $name
+ * @param string $author
+ * @param array $extra_files
+ * @param array $creds
+ */
+function child_themify_create_theme(
+	$slug,
+	$parent,
+	$name = '',
+	$author = '',
+	$extra_files = array(),
+	$creds = array()
+) {
+	$fs = child_themify_get_fs_object( (array) $creds );
+	if ( ! $fs ) {
+		throw new InvalidArgumentException( esc_html__( 'Could not write to server! Please check your credentials and try again.', 'child-themify' ), 1 );
+	}
+
+	$parent_directory    = $parent->get_stylesheet_directory();
+	$new_theme_directory = trailingslashit( get_theme_root() ) . $slug;
+
+	if ( ! child_themify_mkdir_p( $fs, $new_theme_directory ) ) {
+		throw new LogicException( esc_html__( 'Could not create new theme directory!', 'child-themify' ), 2 );
+	}
+
+	$new_stylesheet = trailingslashit( $new_theme_directory ) . 'style.css';
+	if ( ! $fs->touch( $new_stylesheet ) ) {
+		throw new LogicException( esc_html__( 'Could not create new theme stylesheet!', 'child-themify' ), 3 );
+	}
+
+	$stylesheet_contents = child_themify_get_stylesheet_contents(
+		$name,
+		$author,
+		$parent->get( 'Name' ),
+		$parent->get_stylesheet(),
+		child_themify_relative_url(
+			trailingslashit( get_theme_root_uri() ) . "$slug/style.css",
+			trailingslashit( $parent->get_stylesheet_directory_uri() ) . 'style.css'
+		)
+	);
+	if ( ! $fs->put_contents( $new_stylesheet, $stylesheet_contents ) ) {
+		throw new LogicException( esc_html__( 'Could not write to the new theme stylesheet!', 'child-themify' ), 4 );
+	}
+
+	if ( $fs->exists( "$parent_directory/screenshot.png" ) ) {
+		$fs->copy( "$parent_directory/screenshot.png", "$new_theme_directory/screenshot.png" );
+	}
+
+	$functions = trailingslashit( $new_theme_directory ) . 'functions.php';
+	if ( ! $fs->touch( $functions ) ) {
+		throw new LogicException( esc_html__( 'Could not create the new functions.php file!', 'child-themify' ), 5 );
+	}
+	$fs->put_contents( $functions, "<?php\n" );
+
+	foreach ( $extra_files as $extra_file ) {
+		if ( false !== strpos( $extra_file, '../' ) ) {
+			continue;
+		}
+		$original = "$parent_directory/$extra_file";
+		$target   = "$new_theme_directory/$extra_file";
+		if ( ! $fs->exists( $original ) ) {
+			continue;
+		}
+		if ( ! $fs->exists( dirname( $target ) ) && ! child_themify_mkdir_p( $fs, dirname( $target ) ) ) {
+			continue;
+		}
+		$fs->copy( "$parent_directory/$extra_file", "$new_theme_directory/$extra_file" );
+	}
+}
+
+/**
+ * Returns a relative url from one URL to another
+ *
+ * This assumes the domain is the same
+ *
+ * @param string $from
+ * @param string $to
+ *
+ * @return string
+ */
+function child_themify_relative_url( $from, $to ) {
+	$from = parse_url( $from, PHP_URL_PATH );
+	$to   = parse_url( $to, PHP_URL_PATH );
+	$from = explode( '/', $from );
+	$to   = explode( '/', $to );
+	$rel  = $to;
+	foreach ( $from as $pos => $part ) {
+		if ( isset( $to[ $pos ] ) && $to[ $pos ] === $part ) {
+			array_shift( $rel );
+		} else {
+			$remaining = count( $from ) - $pos;
+			if ( $remaining > 1 ) {
+				$pad_length = ( count( $rel ) + $remaining - 1 ) * - 1;
+				$rel        = array_pad( $rel, $pad_length, '..' );
+			} else {
+				$rel[0] = './' . $rel[0];
+			}
+			break;
+		}
+	}
+
+	return implode( '/', array_filter( $rel ) );
+}
+
+/**
+ * Get the contents of the new stylesheet
+ *
+ * @param string $name
+ * @param string $author
+ * @param string $parent_name
+ * @param string $parent_stylesheet
+ * @param string $import
+ *
+ * @return string
+ */
+function child_themify_get_stylesheet_contents( $name, $author, $parent_name, $parent_stylesheet, $import ) {
+	/* translators: The placeholder is the parent theme's name */
+	$description = sprintf( esc_html__( 'A child theme of %s', 'child-themify' ), $parent_name );
+	if ( $author ) {
+		$description .= "\nAuthor: $author";
+	}
+	$stylesheet_contents = <<<EOF
+/*
+Theme Name: $name
+Version: 1.0
+Description: $description
+Template: $parent_stylesheet
+*/
+
+@import url("$import");
+
+EOF;
+
+	return $stylesheet_contents;
+}
+
+/**
+ * Recursive mkdir using the WP filesystem wrapper
+ *
+ * @param WP_Filesystem_Base $fs
+ * @param string $dir
+ *
+ * @return bool
+ */
+function child_themify_mkdir_p( $fs, $dir ) {
+	$new = array();
+	while ( ! $fs->exists( $dir ) ) {
+		array_unshift( $new, basename( $dir ) );
+		if ( $dir === dirname( $dir ) ) {
+			// This would mean the filesystem doesn't exist
+			// Probably not a scenario that will be encountered,
+			// but I also want to avoid an infinite loop.
+			return false;
+		}
+		$dir = dirname( $dir );
+	}
+	foreach ( $new as $part ) {
+		if ( ! $fs->mkdir( trailingslashit( $dir ) . $part ) ) {
+			return false;
+		}
+		$dir = trailingslashit( $dir ) . $part;
+	}
+
+	return true;
+}
