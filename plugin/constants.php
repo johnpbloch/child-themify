397fe0b1cf651b70ad17bfb20246d094c4f68607
John P Bloch <me@johnpbloch.com>
2017-09-10 18:40:02 -0500

    Remove old plugin files

--- plugin/constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define( 'CTF_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) . '/' . basename( __FILE__ ) );
-define( 'CTF_URL', WP_PLUGIN_URL . '/' . basename( dirname( __FILE__ ) ) . '/' . basename( __FILE__ ) );
-define( 'CTF_VERSION', '%%VERSION%%' );
