6fe1a3bb4eb5b424f79bdc49c2f8c1945cdbdf65
John P Bloch <me@johnpbloch.com>
2017-09-10 21:46:42 -0500

    First pass at build script

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+VER=$(git tag | sort -Vr | head -n1)
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -v|--version)
+    VER="$2"
+    shift
+    ;;
+esac
+shift
+done
+
+if [ ! -e build/svn ]; then
+    svn co https://plugins.svn.wordpress.org/child-themify/ build/svn
+fi
+
+npm run clean
+npm run setup
+npm run build:react
+
+mkdir -p build/plugin/assets/js
+mkdir -p build/plugin/assets/css
+
+sed -e "s/{{VERSION}}/$VER/g" < child-themify.php > build/plugin/child-themify.php
+sed -e "s/{{VERSION}}/$VER/g" < readme.txt > build/plugin/readme.txt
+
+cp -r includes build/plugin/
+
+cp assets/css/child-themify.css build/plugin/assets/css/
+cp assets/js/child-themify.js build/plugin/assets/js/
+
+rm -r build/svn/trunk
+
+cp -r build/plugin build/svn/trunk
+
+if [ -e "build/svn/tags/$VER" ]; then
+    rm -r "build/svn/tags/$VER"
+fi
+
+cp -r build/svn/trunk "build/svn/tags/$VER"
